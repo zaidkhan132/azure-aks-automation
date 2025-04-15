c3b2f41b733da5b648400f5486dea98f918b2fd7
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-15 16:42:02 +0530

    Update variables.tf

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -1,6 +1,33 @@
-variable "cluster_name" {}
-variable "resource_group_name" {}
-variable "node_group" {}
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group for the AKS cluster"
+}
+
+variable "location" {
+  description = "Azure region for the AKS cluster"
+}
+
+variable "node_group" {
+  description = "Name of the node group in the AKS cluster"
+}
+
 variable "node_count" {
-  type = number
+  description = "Number of nodes in the node group"
+  type        = number
+}
+
+variable "node_group_label" {
+  description = "Label for the node group"
+  default     = "custom-group"
+}
+
+variable "environment" {
+  description = "Environment for the resources (dev, prod, staging)"
+}
+
+variable "owner" {
+  description = "Owner of the resources"
 }
