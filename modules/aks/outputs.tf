b096b07cd2a379f18e491b72bc8af18d89fc810d
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-15 16:42:24 +0530

    Update outputs.tf

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,4 +1,10 @@
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
+  description = "Admin kubeconfig for the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive   = true
+}
+
+output "aks_cluster_name" {
+  description = "The name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
 }
