43983d1aba1d7278c138253b8c4fbec2f7da7fc2
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-15 16:51:24 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,13 +1,31 @@
+# envs/dev/main.tf
+
 provider "azurerm" {
   features {}
 }
 
-module "aks" {
-  source = "../../modules/aks"
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+}
 
-  cluster_name        = "dev-aks-cluster"
-  resource_group_name = "dev-resource-group"
-  node_group          = "dev-node-group"
-  node_count          = 2
+variable "resource_group_name" {
+  description = "Name of the resource group for the AKS cluster"
+}
+
+variable "node_group" {
+  description = "Name of the node group in the AKS cluster"
+}
+
+variable "node_count" {
+  description = "Number of nodes in the node group"
+  type        = number
+}
+
+module "aks" {
+  source              = "../../modules/aks"
+  cluster_name        = var.cluster_name
+  resource_group_name = var.resource_group_name
+  node_group          = var.node_group
+  node_count          = var.node_count
 }
 
