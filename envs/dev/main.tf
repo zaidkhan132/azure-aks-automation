92c25a769cc1f06b67e469e44e8bed2b2cf93698
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-15 16:48:08 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -7,9 +7,7 @@ module "aks" {
 
   cluster_name        = "dev-aks-cluster"
   resource_group_name = "dev-resource-group"
-  location            = "East US"
   node_group          = "dev-node-group"
   node_count          = 2
-  environment         = "dev"
-  owner               = "Team-Dev"
 }
+
