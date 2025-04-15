877c04e8f4a8a3425e3be8eac7034ba282b36e76
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-15 16:24:06 +0530

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
-  location            = var.location
+  location            = "EAST US"
   resource_group_name = var.resource_group_name
   dns_prefix          = "${var.cluster_name}-dns"
 
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = var.vm_size
+    vm_size    = "Standard_LRS
   }
 
   identity {
@@ -20,7 +20,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_kubernetes_cluster_node_pool" "custom_pool" {
   name                  = var.node_group
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = var.vm_size
+  vm_size               = "Standard_LRS"
   node_count            = var.node_count
   mode                  = "User"
   node_labels           = { "purpose" = "custom" }
