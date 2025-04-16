a0646383fad5f1a46c17b78f8eab4c7908d660e3
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-16 10:42:23 +0530

    Update variables.tf

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -1,16 +1,29 @@
 variable "cluster_name" {
   description = "Name of the AKS cluster"
+  type        = string
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group for the AKS cluster"
+  type        = string
 }
 
-variable "node_group" {
-  description = "Name of the node group in the AKS cluster"
+variable "location" {
+  description = "Azure region where the AKS cluster will be deployed"
+  type        = string
 }
 
-variable "node_count" {
-  description = "Number of nodes in the node group"
-  type        = number
+variable "tags" {
+  description = "Tags for the Azure resources"
+  type        = map(string)
+}
+
+variable "node_pools" {
+  description = "Map of node pools and their configurations"
+  type = map(object({
+    vm_size    = string
+    node_count = number
+    mode       = string
+    tags       = map(string)
+  }))
 }
