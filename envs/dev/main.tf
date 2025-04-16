e7a7297663a8995b18aa57a104530739a4c6d79d
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-16 10:44:34 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,31 +1,44 @@
-# envs/dev/main.tf
-
 provider "azurerm" {
   features {}
 }
 
-variable "cluster_name" {
-  description = "Name of the AKS cluster"
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group for the AKS cluster"
-}
-
-variable "node_group" {
-  description = "Name of the node group in the AKS cluster"
-}
-
-variable "node_count" {
-  description = "Number of nodes in the node group"
-  type        = number
-}
-
 module "aks" {
   source              = "../../modules/aks"
-  cluster_name        = var.cluster_name
-  resource_group_name = var.resource_group_name
-  node_group          = var.node_group
-  node_count          = var.node_count
-}
+  cluster_name        = "dev-aks-cluster"
+  resource_group_name = "dev-resource-group"
+  location            = "East US"
+  tags = {
+    Environment = "Development"
+    Owner       = "Team-Dev"
+  }
 
+  node_pools = {
+    default = {
+      vm_size    = "Standard_B2s"
+      node_count = 2
+      mode       = "System"
+      tags       = {
+        Environment = "Development"
+        Owner       = "Team-Dev"
+      }
+    }
+    infra = {
+      vm_size    = "Standard_D4s_v3"
+      node_count = 3
+      mode       = "User"
+      tags       = {
+        Environment = "Development"
+        Owner       = "Infrastructure"
+      }
+    }
+    apps = {
+      vm_size    = "Standard_DS3_v2"
+      node_count = 1
+      mode       = "User"
+      tags       = {
+        Environment = "Development"
+        Owner       = "Applications"
+      }
+    }
+  }
+}
