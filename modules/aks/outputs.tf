95fac516ed39d77ee4d2bb690580adee65764e0a
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-16 10:42:51 +0530

    Update outputs.tf

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,4 +1,15 @@
-output "aks_cluster_name" {
-  description = "The name of the AKS cluster"
+output "kube_admin_config" {
+  description = "Admin kubeconfig for the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+  sensitive   = true
+}
+
+output "aks_name" {
+  description = "Name of the AKS cluster"
   value       = azurerm_kubernetes_cluster.aks.name
+}
+
+output "node_pools" {
+  description = "Node pools created in the AKS cluster"
+  value       = azurerm_kubernetes_cluster_node_pool.node_pool
 }
