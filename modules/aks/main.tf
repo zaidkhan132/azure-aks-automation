ffcccb822800fc10674019f54755f8c7c092946d
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-16 10:41:32 +0530

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,33 +1,28 @@
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
-  location            = "East US"                # Static value
+  location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = "${var.cluster_name}-dns"
 
   default_node_pool {
     name       = "default"
-    node_count = 1                              # Static default node count
-    vm_size    = "Standard_B2s"                # Static VM size
+    node_count = 1
+    vm_size    = "Standard_B2s"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  tags = {
-    Environment = "Development"
-    Owner       = "Team-Ilens"
-  }
+  tags = var.tags
 }
 
 resource "azurerm_kubernetes_cluster_node_pool" "node_pool" {
+  for_each              = var.node_pools
   name                  = var.node_group
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  node_count            = var.node_count
-  vm_size               = "Standard_B2s"       # Static VM size
-  mode                  = "User"
-  tags = {
-    Environment = "Development"
-    Owner       = "Team-Ilens"
-  }
+  vm_size               = each.value.vm_size
+  node_count            = each.value.node_count
+  mode                  = each.value.mode
+  tags                  = each.value.tags
 }
