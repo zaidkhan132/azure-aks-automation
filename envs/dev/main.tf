810d2cf6758b26f86dc768199a9abed6867b626e
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-17 13:43:01 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -13,7 +13,11 @@ module "aks" {
   cluster_name        = var.cluster_name
   resource_group_name = var.resource_group_name
   location            = var.location
+  tags                = var.tags
+  node_group          = var.node_group
+  node_count          = var.node_count
+  node_pools          = var.node_pools
   infra_pool_count    = var.infra_pool_count
   core_pool_count     = var.core_pool_count
-  tags                = var.tags
 }
+
