4e0b801cc9fb0c9af0639880ce993fd036e93641
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-17 10:25:52 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,44 +1,11 @@
-provider "azurerm" {
-  features {}
-}
-
 module "aks" {
-  source              = "../../modules/aks"
-  cluster_name        = "dev-aks-cluster"
-  resource_group_name = "dev-resource-group"
-  location            = "East US"
-  tags = {
-    Environment = "Development"
-    Owner       = "Team-Dev"
-  }
+  source = "../../modules/aks"
 
-  node_pools = {
-    default = {
-      vm_size    = "Standard_B2s"
-      node_count = 2
-      mode       = "System"
-      tags       = {
-        Environment = "Development"
-        Owner       = "Team-Dev"
-      }
-    }
-    infra = {
-      vm_size    = "Standard_D4s_v3"
-      node_count = 3
-      mode       = "User"
-      tags       = {
-        Environment = "Development"
-        Owner       = "Infrastructure"
-      }
-    }
-    apps = {
-      vm_size    = "Standard_DS3_v2"
-      node_count = 1
-      mode       = "User"
-      tags       = {
-        Environment = "Development"
-        Owner       = "Applications"
-      }
-    }
-  }
+  cluster_name         = var.cluster_name
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  tags                 = var.tags
+  node_group           = var.node_group
+  node_count           = var.node_count
+  node_pools           = var.node_pools
 }
