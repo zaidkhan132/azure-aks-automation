88859189b94042a24eb2a4eb28a9b22d344f6498
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-17 15:21:34 +0530

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,27 +1,14 @@
-
-  default_node_pool {
-    name       = var.node_group
-    node_count = var.node_count
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   resource_group_name = var.resource_group_name
   location            = var.location
   tags                = var.tags
 
-  dns_prefix          = var.dns_prefix
-
   default_node_pool {
     name       = var.node_group
     node_count = var.node_count
     vm_size    = "Standard_DS2_v2"
+    mode       = "System"                    # Required for default node pools
   }
 
   identity {
@@ -34,7 +21,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "infra_pool" {
   name                  = "infra_pool"
   node_count            = var.infra_pool_count
   vm_size               = "Standard_DS2_v2"
-  mode                  = "User"
+  mode                  = "User"             # Node pool type (User/System)
   tags                  = var.tags
 }
 
@@ -43,6 +30,6 @@ resource "azurerm_kubernetes_cluster_node_pool" "core_pool" {
   name                  = "core_pool"
   node_count            = var.core_pool_count
   vm_size               = "Standard_DS2_v2"
-  mode                  = "User"
+  mode                  = "User"             # Node pool type (User/System)
   tags                  = var.tags
 }
