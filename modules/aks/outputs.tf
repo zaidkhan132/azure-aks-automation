67aad498664c366203a231167c100a170a0520c0
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-17 15:22:32 +0530

    Update outputs.tf

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,14 +1,12 @@
-output "kube_admin_config" {
-  description = "Admin kubeconfig for the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
-  sensitive   = true
+output "kubernetes_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks.id
 }
 
-output "aks_name" {
-  description = "Name of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.name
-}
-
-output "node_pools" {
+output "infra_pool_name" {
   value = azurerm_kubernetes_cluster_node_pool.infra_pool.name
 }
+
+output "core_pool_name" {
+  value = azurerm_kubernetes_cluster_node_pool.core_pool.name
+}
+
