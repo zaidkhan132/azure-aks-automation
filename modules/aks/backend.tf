6c5bc4d5f27f236c6c9d14c6056ef146711d4ab1
zaid bin azeez <zaidbinazeez123@gmail.com>
2025-04-22 10:42:31 +0530

    dev modification

--- modules/aks/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Demo-Resource"
-    storage_account_name = "zaid01storageaccount"
-    container_name       = "zaidcontainer"
-    key                  = "ilens-aks-dev-tfstate"
-  }
-}
-
