800f760c4a8b055ec400b1ce8189805bcb4af638
zaid bin azeez <zaidbinazeez123@gmail.com>
2025-04-22 10:40:08 +0530

    dev modification

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,4 +1,3 @@
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   resource_group_name = var.resource_group_name
@@ -6,31 +5,28 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.dns_prefix
   tags                = var.tags
 
-  default_node_pool {
-    name       = var.node_group
-    node_count = var.node_count
-    vm_size    = "Standard_DS2_v2"
-  }
-
   identity {
     type = "SystemAssigned"
   }
 }
 
+# Infrastructure Node Pool
 resource "azurerm_kubernetes_cluster_node_pool" "infra_pool" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
   name                  = "infrapool"
   node_count            = var.infra_pool_count
   vm_size               = "Standard_DS2_v2"
-  mode                  = "User"             # Node pool type (User/System)
+  mode                  = "User"
   tags                  = var.tags
 }
 
+# Core Node Pool
 resource "azurerm_kubernetes_cluster_node_pool" "core_pool" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
   name                  = "corepool"
   node_count            = var.core_pool_count
   vm_size               = "Standard_DS2_v2"
-  mode                  = "User"             # Node pool type (User/System)
+  mode                  = "User"
   tags                  = var.tags
 }
+
